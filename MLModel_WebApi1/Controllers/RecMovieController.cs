b75a6f8abccbec180c387f5e37e4a71904276c49
VN The Boss Baby <93357243+QuDaMyker@users.noreply.github.com>
2024-01-07 15:42:47 +0700

    Add project files.

---
+++ MLModel_WebApi1/Controllers/RecMovieController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MLModel_WebApi1.Controllers
+{
+    public class RecMovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
